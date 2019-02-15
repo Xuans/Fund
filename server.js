8f7f8577459fa8169ff268f3ddbfc87cd549df19
KinSueng <594610825@qq.com>
2019-02-15 09:58:57 +0800

    调通websocket
    调通websocket

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ const PORT = '3000';
 
 
 // Dependences
-const os = require('os');
 const Koa = require('koa');
 const serve = require('koa-static');
-const proxyPass = require('@junyiz/koa-proxy-pass');
+const proxy = require('@junyiz/koa-proxy-pass');
 const router = require('koa-router')();
 const koaBody = require('koa-body');
 const httpRequest = require('request');
@@ -35,14 +34,14 @@ const mkdir = (dirname) => {
 
 	list.push(dirname);
 	while (
-		(parent = path.dirname(list[list.length - 1]))!==list[list.length - 1]
+		(parent = path.dirname(list[list.length - 1])) !== list[list.length - 1]
 		&& parent
 	) {
 		list.push(parent);
 	}
 
-	list.reverse().forEach(p=>{
-		if(!fs.existsSync(p)){
+	list.reverse().forEach(p => {
+		if (!fs.existsSync(p)) {
 			fs.mkdirSync(p);
 		}
 	});
@@ -53,10 +52,10 @@ router.get('/', async (ctx, next) => {
 	ctx.response.type = 'html';
 	ctx.response.body = await fs.readFile(LOCAL_PATH + WELCOME_PAGE_PATH, 'utf8');
 });
-
+//uploadImg
 router.post('/uploadImg', async (ctx, next) => {
-	const date=new Date();
-	const tmpdir = `${__dirname}/assets/img/${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`;
+	const date = new Date();
+	const tmpdir = `${__dirname}/assets/img/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
 	const filePaths = [];
 	const files = ctx.request.files || ctx.files.body || {};
 
@@ -78,7 +77,7 @@ router.post('/uploadImg', async (ctx, next) => {
 		}
 	}
 
-	ctx.response.redirect(filePaths[0].replace(__dirname,''));
+	ctx.response.redirect(filePaths[0].replace(__dirname, ''));
 	// ctx.response.type = 'image';
 	// ctx.response.body = await fs.readFile(filePaths[0]);
 	// ctx.type = 'json';
@@ -91,19 +90,40 @@ router.post('/uploadImg', async (ctx, next) => {
 const app = new Koa();
 app.use(cors());
 
+//proxy
+app.use(proxy({
+	host: 'wss://sshibiocbo.jin10.com:8082/socket.io',
+	match: /aaaa/
+}));
+
 //parser
 app.use(koaBody({ multipart: true }));
 
-//websocket
-//const wsProxy = proxy('wss://stats-node3.jin10.com:8001/', { changeOrigin: true })
-
-//app.use(wsProxy)
-
-//setting routers
 app.use(main);
 
 app.use(router.routes());
+//app.listen(PORT);
 
-const server = app.listen(PORT);
+const socket = require('socket.io');
+const server = require('http').createServer(app.callback());
+const io = socket(server);
+io.on('connection', ins => {
+	console.log('初始化成功！下面可以用socket绑定事件和触发事件了');
 
-//server.on('upgrade', wsProxy.upgrade) // <-- subscribe to http 'upgrade'
+	debugger;
+	const remote = socket('wss://sshibiocbo.jin10.com:8082/socket.io/?EIO=3&transport=websocket&sid=aYiPZwo79aGiCcWLUAiE');
+
+	remote.on('connect', () => {
+		ins.emit('getMsg', 'success');
+	})
+
+	ins.on('send', data => {
+		console.log('客户端发送的内容：', data);
+		ins.emit('getMsg', '我是返回的消息... ...');
+	})
+
+	setTimeout(() => {
+		ins.emit('getMsg', '我是初始化3s后的返回消息... ...')
+	}, 3000)
+});
+server.listen(PORT);
